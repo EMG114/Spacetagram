2458b229c1b585b8e75229d65b8cdbb2edc48239
Erica <erica@ericas-mac-mini.local>
2018-01-08 21:04:09 -0500

    "working on Sign UP VC functions"

--- Spacetagram/SignUpViewController.swift
+++ Spacetagram/SignUpViewController.swift
@@ -10,12 +10,104 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var scrollview: UIScrollView!
+    
+
+    
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var repeatPassTextField: UITextField!
+    
+    @IBOutlet weak var fullnameTextField: UITextField!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBOutlet weak var websiteTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    
+    
+    //reset default size
+   var scrollViewHeight: CGFloat = 0
+    
+    //keyboard frame size
+   var keyboard = CGRect()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        //scrollview frame size
+        scrollview.frame = CGRect(0,0,self.view.frame.width, self.view.frame.width)
+        scrollview.contentSize.height = self.view.frame.height
+        scrollViewHeight = scrollview.frame.size.height
+        
+        NotificationCenter.default.addObserver(self, selector: Selector(("showKeyboard:")), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+         NotificationCenter.default.addObserver(self, selector: Selector(("hideKeyboard:")), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        let hideTap = UITapGestureRecognizer(target: self, action: Selector(("hideKeyboardTapped:")))
+        hideTap.numberOfTapsRequired = 1
+        self.view.isUserInteractionEnabled = true
+        self.view.addGestureRecognizer(hideTap)
+        
+    }
+    
+    func hideKeyboardTapped(recognizer:UITapGestureRecognizer) {
+        
+        self.view.endEditing(true)
+        
+        }
+    
+    
+    
+    func showKeyboard(notification:NSNotification){
+        
+        //define keyboard size
+        keyboard = ((notification.userInfo?[UIKeyboardFrameEndUserInfoKey]! as AnyObject).cgRectValue)!
+        
+        UIView.animate(withDuration: 0.4) { () -> Void in
+            self.scrollview.frame.size.height = self.scrollViewHeight - self.keyboard.height
+        }
+        
+    }
+    
+    
+    
+    func hideKeyboard(notification:NSNotification){
+        
+        UIView.animate(withDuration: 0.4) { () -> Void in
+            self.scrollview.frame.size.height = self.view.frame.height
+            
+        }
+        
+    }
+    
+    
+    
+    
+
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        print("sign up")
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
 
+
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -32,4 +124,10 @@ class SignUpViewController: UIViewController {
     }
     */
 
+}
+
+extension CGRect {
+    init(_ x:CGFloat, _ y:CGFloat, _ w:CGFloat, _ h:CGFloat) {
+        self.init(x:x, y:y, width:w, height:h)
+    }
 }
