83f2d3a90a7ca445c27d11ebeec93edbd3739d07
Erica <egutierrez411@gmail.com>
2018-03-15 15:47:30 -0400

    added picture file

---
+++ Spacetagram/PictureCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PictureCollectionViewCell.swift
+//  Spacetagram
+//
+//  Created by Erica on 3/15/18.
+//  Copyright © 2018 Erica. All rights reserved.
+//
+
+import UIKit
+
+class PictureCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pictureImage: UIImageView!
+}
