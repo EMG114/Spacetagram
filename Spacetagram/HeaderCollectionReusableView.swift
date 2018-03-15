76d481479f44bec88749ee47a2d66b9adf7bcee7
Erica <egutierrez411@gmail.com>
2018-03-15 15:11:18 -0400

    adding collectionvc files

---
+++ Spacetagram/HeaderCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderCollectionReusableView.swift
+//  Spacetagram
+//
+//  Created by Erica on 3/15/18.
+//  Copyright © 2018 Erica. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+        
+}
