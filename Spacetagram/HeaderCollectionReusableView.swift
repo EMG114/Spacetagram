47472b1f9d889ec8d5e6fda73d76b95f3298c400
Erica <egutierrez411@gmail.com>
2018-03-15 15:26:44 -0400

    adding otlets

--- Spacetagram/HeaderCollectionReusableView.swift
+++ Spacetagram/HeaderCollectionReusableView.swift
@@ -9,5 +9,31 @@
 import UIKit
 
 class HeaderCollectionReusableView: UICollectionReusableView {
-        
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var websiteTextField: UITextView!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var posts: UILabel!
+    
+    @IBOutlet weak var followers: UILabel!
+    
+    @IBOutlet weak var following: UILabel!
+    
+    @IBOutlet weak var postsTitle: UILabel!
+    
+    @IBOutlet weak var followersTitle: UILabel!
+    
+    @IBOutlet weak var followingTitle: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
+    
+    
+    
+    
+    
 }
